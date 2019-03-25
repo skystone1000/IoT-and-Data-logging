f9d228d65f7eaa20b87014cbfd3b18c8af06b4e4
skystone1000 <adityaspmahajan@gmail.com>
2019-03-25 12:29:56 +0530

    ADXL345 And Hall Effect sensor Integrated

--- 06.Display/display final/01AdxlHall.py
+++ 06.Display/display final/01AdxlHall.py
@@ -1,38 +1,26 @@
 import csv, os
 from time import gmtime, strftime
-
 # rpm
 import RPi.GPIO as GPIO
 import time
-# rpm end
-
 # adxl code
 import smbus
 import time
 from time import sleep
-
 # display
 import time, datetime, sys
 import pygame, time, os, csv
 from pygame.locals import *
 
-# display end
-
 # Globals
 rpm = 0
 speed = 0
 coolantTemp = 20
-
 throttlePosition = 0
 timingAdvance = 0
-
 gear = 0
 black = (0,0,0)
 WHITE = (255,255,255)
-
-
-
-
 
 # rpm
 GPIO.setmode(GPIO.BCM)
@@ -68,7 +56,6 @@ MEASURE = 0x08
 AXES_DATA = 0x32
 # adxl code end
 
-
 startTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
 # rpm
@@ -82,16 +69,13 @@ elapse = 0.00
 multiplier = 0
 
 start = time.time()
-
 GPIO.setup(hall, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # display
 # Set up the window. If piTFT flag is set, set up the window for the screen. Elsecreate it normally for use on normal monitor.
-
 pygame.init()
 pygame.mouse.set_visible(0)
 windowSurface = pygame.display.set_mode((320, 240))
-
 
 # Helper function to draw the given string at coordinate x,y, relative to center.
 def drawText(string, x, y, font):
@@ -106,38 +90,28 @@ def drawText(string, x, y, font):
     textRect.centery = windowSurface.get_rect().centery + y
     windowSurface.blit(text, textRect)
 
-
 # Set up fonts
 pygame.init()
 readoutFont = pygame.font.Font("Mohave-Regular.ttf", 30)
 labelFont = pygame.font.Font("Mohave-Regular.ttf", 20)
 fpsFont = pygame.font.Font("Mohave-Regular.ttf", 10)
-
 # Set the caption.
 pygame.display.set_caption("TEAM PRAVEG")
-
 # Create a clock object to use so we can log every second.
 clock = pygame.time.Clock()
-
-
 # display end
-
 
 def get_rpm():
     return rpm
 
-
 def get_speed():
     return speed
-
 
 def get_distance():
     return distance
 
-
 def get_elapse():
     return elapse
-
 
 def get_pulse(number):
     global elapse, distance, start, pulse, speed, rpm, multiplier
@@ -155,12 +129,8 @@ def get_pulse(number):
     rpm = 1 / elapse * 60
     # below is the converter from kmph to mph
     # speed = speed*0.621371
-
     start = time.time()
-
-
 # rpm end
-
 
 # adxl
 class ADXL345:
@@ -181,11 +151,9 @@ class ADXL345:
     # set the measurement range for 10-bit readings
     def setRange(self, range_flag):
         value = bus.read_byte_data(self.address, DATA_FORMAT)
-
         value &= ~0x0F;
         value |= range_flag;
         value |= 0x08;
-
         bus.write_byte_data(self.address, DATA_FORMAT, value)
 
     # returns the current reading from the sensor for each axis
@@ -195,77 +163,62 @@ class ADXL345:
     #    True           : result is returned in gs
     def getAxes(self, gforce=False):
         bytes = bus.read_i2c_block_data(self.address, AXES_DATA, 6)
-
         x = bytes[0] | (bytes[1] << 8)
         if (x & (1 << 16 - 1)):
             x = x - (1 << 16)
-
         y = bytes[2] | (bytes[3] << 8)
         if (y & (1 << 16 - 1)):
             y = y - (1 << 16)
-
         z = bytes[4] | (bytes[5] << 8)
         if (z & (1 << 16 - 1)):
             z = z - (1 << 16)
-
         x = x * SCALE_MULTIPLIER
         y = y * SCALE_MULTIPLIER
         z = z * SCALE_MULTIPLIER
-
         if gforce == False:
             x = x * EARTH_GRAVITY_MS2
             y = y * EARTH_GRAVITY_MS2
             z = z * EARTH_GRAVITY_MS2
-
         x = round(x, 4)
         y = round(y, 4)
         z = round(z, 4)
-
         return {"x": x, "y": y, "z": z}
 
-
 # log the data
-
 # Function to create a csv with the specified header.
 def createLog(header):
     # Write the header of the csv file.
-    with open('/home/pi/all/' + str(startTime) + '.csv', 'wb') as f:
+    with open('/home/pi/' + str(startTime) + '.csv', 'wb') as f:
         w = csv.writer(f)
         w.writerow(header)
 
-
 # Function to append to the current log file.
 def updateLog(data):
-    with open('/home/pi/all/' + str(startTime) + '.csv', 'a') as f:
+    with open('/home/pi/' + str(startTime) + '.csv', 'a') as f:
         w = csv.writer(f)
         w.writerow(data)
-
 
 # Function to close the log and rename it to include end time.
 def closeLog():
     endTime = datetime.datetime.today().strftime('%Y%m%d%H%M%S')
-    os.rename('home/pi/all/' + str(startTime) + '.csv', 'logs/' + startTime + "_" +
+    os.rename('home/pi/' + str(startTime) + '.csv', 'logs/' + startTime + "_" +
               endTime + '.csv')
-
 
 if __name__ == "__main__":
     # if run directly we'll just create an instance of the class and output
     # the current readings
     adxl345 = ADXL345()
-
     time.sleep(1)
     GPIO.add_event_detect(hall, GPIO.FALLING, callback=get_pulse, bouncetime=20)
-
     #header = ["time", "axisX", " axisY ", " axisZ", "Rpm", "Speed", "Distance"]
     #createLog(header)
-
     while (True):
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
 
-                # Draw the RPM readout and label.
+        # Draw the RPM readout and label.
         drawText(str(rpm), -5, -10, "readout")
         drawText("RPM", -5, 20, "label")
 
@@ -297,16 +250,11 @@ if __name__ == "__main__":
         #	drawText( str (engineLoad) + " %" , 0, -145, "readout" )
         #	drawText( "Load" , 0, -110, "label" )
 
-
-
         # Update the clock.
         dt = clock.tick()
-
         # draw the window onto the screen
         pygame.display.update()
-
         print('rpm:{0:.1f} speed:{1:.0f} distance:{2}'.format(rpm, speed,distance))  # this only prints rpm, speed, and distance
-
         axes = adxl345.getAxes(True)
         print("ADXL345 on address :")
         print("   x = {:.3f}G".format(axes['x']))
@@ -322,18 +270,13 @@ if __name__ == "__main__":
         Rpm = rpm
         Speed = speed
         Distance = distance
-
         data = [strftime("%Y-%m-%d %H:%M:%S", gmtime()), axisX, axisY, axisZ, Rpm, Speed, Distance]
         updateLog(data)
         windowSurface.fill(black)
-        
         time.sleep(.5)  # trying to make the display update with each pass of the hall sensor
-
         try:
             print("")
-
         except KeyboardInterrupt:
             print('Guess that\'s the end of your trip, huh?')
             closeLog()
             GPIO.cleanup()
-                 
